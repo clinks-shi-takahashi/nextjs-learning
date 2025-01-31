242d69f37468e1b8696bd27cc1c879c128776f3a
clinks-shi-takahashi <shi-takahashi@clinks.jp>
2025-01-31 17:09:42 +0900

    [Chapter 16]Adding Metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -3,12 +3,16 @@ import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
 import { Suspense } from "react";
-import { fetchCardData } from "@/app/lib/data";
 import {
   RevenueChartSkeleton,
   LatestInvoicesSkeleton,
   CardsSkeleton,
 } from "@/app/ui/skeletons";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "dashboard",
+};
 
 export default async function Page() {
   return (
